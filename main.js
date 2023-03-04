2dbb57d1aa4ba6d401cd3caf6b0a46d6179d6325
Konrad Banasik <konrad2666@gmail.com>
2023-03-04 16:22:48 +0100

    Fix style of img

--- main.js
+++ main.js
@@ -2,4 +2,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const scroller = new Scroller("#root");
 
   document.addEventListener("wheel", (event) => scroller.listenScroll(event));
+
+  document.activeElement("swipeUp", () => scroller.scroll(1));
+  document.activeElement("swipeDown", () => scroller.scroll(-1));
 });
